90e39dd0807509925ca9f08d16923e89f666dca9
lakshmipriya5899 <119044303+lakshmipriya5899@users.noreply.github.com>
2022-11-25 19:21:18 +0530

    Update and rename lakshmi1.py to user-link-creation.py

--- user-link-creation.py
+++ user-link-creation.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
  
  
-url = 'https://ful.io'
+url= input("Enter username:")
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
  
